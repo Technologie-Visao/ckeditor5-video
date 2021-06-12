5502eb477dc0a8ac1e24a1298b019f0dc4b1caf5
Alex Aubé <alexdaube@users.noreply.github.com>
2021-06-12 18:20:10 -0400

    Upgrade to 28.0.0 ckeditor deps (#16)

--- src/index.js
+++ src/index.js
@@ -1,37 +1,17 @@
-import AutoVideo from './autovideo';
-import Video from './video';
-import VideoEditing from "./video/videoediting";
-import VideoResize from './videoresize';
-// import VideoInsert from './videoinsert';
-// import VideoInsertUI from './videoinsert/videoinsertui';
-import VideoResizeButtons from './videoresize/videoresizebuttons';
-import VideoResizeEditing from './videoresize/videoresizeediting';
-import VideoResizeHandles from './videoresize/videoresizehandles';
-import VideoStyle from './videostyle';
-import VideoStyleEditing from './videostyle/videostyleediting';
-import VideoStyleUI from './videostyle/videostyleui';
-import VideoToolbar from './videotoolbar';
-import VideoUpload from './videoupload';
-import VideoUploadEditing from './videoupload/videouploadediting';
-import VideoUploadProgress from './videoupload/videouploadprogress';
-import VideoUploadUI from './videoupload/videouploadui';
-
-export default {
-    AutoVideo,
-    Video,
-    VideoEditing,
-    // VideoInsert,
-    // VideoInsertUI,
-    VideoResize,
-    VideoResizeButtons,
-    VideoResizeEditing,
-    VideoResizeHandles,
-    VideoStyle,
-    VideoStyleEditing,
-    VideoStyleUI,
-    VideoToolbar,
-    VideoUpload,
-    VideoUploadEditing,
-    VideoUploadProgress,
-    VideoUploadUI,
-};
+export { default as AutoVideo } from './autovideo';
+import  { default as Video } from './video';
+import  { default as VideoEditing } from "./video/videoediting";
+import { default as VideoResize } from './videoresize';
+import { default as VideoInsert } from './videoinsert';
+import { default as VideoInsertUI } from './videoinsert/videoinsertui';
+import { default as VideoResizeButtons } from './videoresize/videoresizebuttons';
+import { default as VideoResizeEditing }  from './videoresize/videoresizeediting';
+import { default as VideoResizeHandles } from './videoresize/videoresizehandles';
+import { default as VideoStyle } from './videostyle';
+import { default as VideoStyleEditing } from './videostyle/videostyleediting';
+import { default as VideoStyleUI } from './videostyle/videostyleui';
+import { default as VideoToolbar } from './videotoolbar';
+import { default as VideoUpload } from './videoupload';
+import { default as VideoUploadEditing } from './videoupload/videouploadediting';
+import { default as  VideoUploadProgress } from './videoupload/videouploadprogress';
+import { default as VideoUploadUI } from './videoupload/videouploadui';
